3e494f3b42b685660fbb194969729006d68a2547
seitaro <sei.t11@icloud.com>
2024-02-13 15:06:49 +0900

    def

---
+++ src/tmp-renderer/html/html.ts
@@ -0,0 +1,9 @@
+
+
+export class RootElement {
+    elm: HTMLBodyElement
+
+    constructor(elm: HTMLBodyElement){
+        this.elm = elm
+    }
+}
---
+++ src/tmp-renderer/main.ts
@@ -0,0 +1,4 @@
+import { RootElement } from "./html/html";
+
+
+const root_elm = new RootElement(document.body as HTMLBodyElement)
