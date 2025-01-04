8cbeae3ff6d98e72d437017f713b120d4d5c36ca
sei-12 <sei.t11@icloud.com>
2025-01-05 00:07:08 +0900

    add tests of BookmarkList

--- src/hooks/BookmarkList.test.ts
+++ src/hooks/BookmarkList.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { describe, expect, it, vi } from "vitest";
 import { useBookmarkList } from "./BookmarkList";
 
@@ -11,7 +11,116 @@ describe("BookmarkList", () => {
             onClickRemoveMock,
             onClickCancelMock
         ));
-        
+
         expect(hook.result.current.items).toEqual([]);
     });
+
+    it("case2", async () => {
+        const onClickRemoveMock = vi.fn();
+        const onClickCancelMock = vi.fn();
+
+        const hook = renderHook(() => useBookmarkList(
+            onClickRemoveMock,
+            onClickCancelMock
+        ));
+
+        const items = [
+            { id: 1, title: "title1", url: "url1", desc: "desc1", tags: ["tag1", "tag2"] },
+            { id: 2, title: "title2", url: "url2", desc: "desc2", tags: ["tag3", "tag4"] },
+            { id: 3, title: "title3", url: "url3", desc: "desc3", tags: ["tag5", "tag6"] },
+
+        ]
+        act(() => {
+            hook.result.current.setItems(items)
+        })
+
+        expect(hook.result.current.items).toEqual(items);
+        expect(hook.result.current.getFocusedItem()).toEqual(items[0]);
+        act(() => { hook.result.current.focusDown() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[1]);
+        act(() => { hook.result.current.focusDown() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[2]);
+        act(() => { hook.result.current.focusDown() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[0]);
+    })
+
+    it("case3", async () => {
+        const onClickRemoveMock = vi.fn();
+        const onClickCancelMock = vi.fn();
+
+        const hook = renderHook(() => useBookmarkList(
+            onClickRemoveMock,
+            onClickCancelMock
+        ));
+
+        const items = [
+            { id: 1, title: "title1", url: "url1", desc: "desc1", tags: ["tag1", "tag2"] },
+            { id: 2, title: "title2", url: "url2", desc: "desc2", tags: ["tag3", "tag4"] },
+            { id: 3, title: "title3", url: "url3", desc: "desc3", tags: ["tag5", "tag6"] },
+
+        ]
+        act(() => {
+            hook.result.current.setItems(items)
+        })
+
+        expect(hook.result.current.items).toEqual(items);
+        expect(hook.result.current.getFocusedItem()).toEqual(items[0]);
+        act(() => { hook.result.current.focusUp() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[2]);
+        act(() => { hook.result.current.focusUp() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[1]);
+        act(() => { hook.result.current.focusUp() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[0]);
+    })
+
+    it("case4", async () => {
+        const onClickRemoveMock = vi.fn();
+        const onClickCancelMock = vi.fn();
+
+        const hook = renderHook(() => useBookmarkList(
+            onClickRemoveMock,
+            onClickCancelMock
+        ));
+
+        const items = [
+            { id: 1, title: "title1", url: "url1", desc: "desc1", tags: ["tag1", "tag2"] },
+            { id: 2, title: "title2", url: "url2", desc: "desc2", tags: ["tag3", "tag4"] },
+            { id: 3, title: "title3", url: "url3", desc: "desc3", tags: ["tag5", "tag6"] },
+
+        ]
+
+        act(() => {
+            hook.result.current.setItems(items)
+        })
+
+        expect(hook.result.current.items).toEqual(items);
+        act(() => { hook.result.current.focusUp() })
+        act(() => { hook.result.current.focusUp() })
+        act(() => { hook.result.current.resetFocusIndex() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[0]);
+        act(() => { hook.result.current.focusUp() })
+        act(() => { hook.result.current.focusUp() })
+        act(() => { hook.result.current.focusUp() })
+        act(() => { hook.result.current.resetFocusIndex() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[0]);
+        act(() => { hook.result.current.focusDown() })
+        act(() => { hook.result.current.focusDown() })
+        act(() => { hook.result.current.resetFocusIndex() })
+        expect(hook.result.current.getFocusedItem()).toEqual(items[0]);
+    })
+    it("case5", async () => {
+        const onClickRemoveMock = vi.fn();
+        const onClickCancelMock = vi.fn();
+
+        const hook = renderHook(() => useBookmarkList(
+            onClickRemoveMock,
+            onClickCancelMock
+        ));
+
+        act(() => {
+            hook.result.current.setItems([])
+        })
+
+        expect(hook.result.current.getFocusedItem()).toEqual(undefined);
+    })
 });
