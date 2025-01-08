804b1ffe0ed470e5b72310c5ef4555160d280c08
sei-12 <sei.t11@icloud.com>
2025-01-09 08:33:21 +0900

    add runner windows-2025

--- src-tauri/src/db/test_test.rs
+++ src-tauri/src/db/test_test.rs
@@ -9,6 +9,8 @@
 //     println!("{:?}",path);    
 // }
 
+
+// process didn't exit successfully: `D:\a\skin\skin\src-tauri\target\debug\deps\skin_lib-1f62670fd56f937a.exe` (exit code: 0xc0000139, STATUS_ENTRYPOINT_NOT_FOUND)
 #[test]
 fn test_tauri_mock_app(){
     let app = tauri::test::mock_app();
