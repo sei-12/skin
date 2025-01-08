4fcc5aa21b06c50fce21b7f0d6f1801380cb250c
sei-12 <sei.t11@icloud.com>
2025-01-09 08:07:31 +0900

    update test test

--- src-tauri/src/db/test_test.rs
+++ src-tauri/src/db/test_test.rs
@@ -1,18 +1,19 @@
 
 
 
-#[test]
-fn test_tmp_dir(){
-    use std::env::temp_dir;
-    let path = temp_dir();
-    println!("{:?}",path);
-}
-
+// 通った
 // #[test]
-// fn test_tauri_mock_app(){
-//     let app = tauri::test::mock_app();
-//     println!("{:?}",app);
+// fn test_tmp_dir(){
+//     use std::env::temp_dir;
+//     let path = temp_dir();
+//     println!("{:?}",path);    
 // }
+
+#[test]
+fn test_tauri_mock_app(){
+    let app = tauri::test::mock_app();
+    println!("{:?}",app);
+}
 
 // #[test]
 // fn test_rand() {
