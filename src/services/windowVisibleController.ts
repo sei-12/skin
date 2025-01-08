4ce5049d5b818f459ff870ec431d8015ed3c673d
sei-12 <sei.t11@icloud.com>
2025-01-08 21:42:17 +0900

    update

--- src/services/windowVisibleController.ts
+++ src/services/windowVisibleController.ts
@@ -1,31 +1,31 @@
 
 
-// import { listen } from "@tauri-apps/api/event";
+import { listen } from "@tauri-apps/api/event";
 import { getCurrentWindow } from "@tauri-apps/api/window";
-// import { register } from "@tauri-apps/plugin-global-shortcut";
+import { register } from "@tauri-apps/plugin-global-shortcut";
+
+if ( import.meta.env.MODE === "production" ){
+    getCurrentWindow().setVisibleOnAllWorkspaces(true)
+
+    register("Alt+Z", async (event) => {
+        if (event.state === "Released") {
+            return;
+        }
+
+        const curVisibility = await WindowVisibleController.currentVisible();
+        if (curVisibility) {
+            WindowVisibleController.hide();
+        } else {
+            WindowVisibleController.show();
+        }
+    });
+
+    listen("tauri://blur", async () => {
+        WindowVisibleController.hide();
+    });
+}
 
 
-// getCurrentWindow().setVisibleOnAllWorkspaces(true)
-
-// register("Alt+Z", async (event) => {
-//     if (event.state === "Released") {
-//         return;
-//     }
-
-//     const curVisibility = await WindowVisibleController.currentVisible();
-//     if (curVisibility) {
-//         WindowVisibleController.hide();
-//     } else {
-//         WindowVisibleController.show();
-//         // tagInputBoxHook.inputBoxRef.current?.focus();
-//     }
-// });
-
-// listen("tauri://blur", async () => {
-//     WindowVisibleController.hide();
-//     // 再度開いた時に前回の検索結果などが残らないようにする。
-//     // tagInputBoxHook.setInputedTags([]);
-// });
 
 /**
  * ウィンドウの表示/非表示関係の処理がとてもテストしにくいため、別のモジュールに分ける
@@ -33,14 +33,16 @@ import { getCurrentWindow } from "@tauri-apps/api/window";
 export namespace WindowVisibleController {
 
     export function show() {
-        // const curWinodw = getCurrentWindow()
-        // curWinodw.show()
-        // curWinodw.setFocus()
+        if ( import.meta.env.MODE !== "production" ){ return }
+        const curWinodw = getCurrentWindow()
+        curWinodw.show()
+        curWinodw.setFocus()
     }
 
     export function hide() {
-        // const curWinodw = getCurrentWindow()
-        // curWinodw.hide()
+        if ( import.meta.env.MODE !== "production" ){ return }
+        const curWinodw = getCurrentWindow()
+        curWinodw.hide()
     }
 
     export function currentVisible() {
