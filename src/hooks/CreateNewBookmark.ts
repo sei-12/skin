68b099b9c9a785582c6be91172fe93504e77b9ac
sei-12 <sei.t11@icloud.com>
2025-02-05 17:28:44 +0900

    fix bug

--- src/hooks/CreateNewBookmark.ts
+++ src/hooks/CreateNewBookmark.ts
@@ -55,7 +55,9 @@ export function useCreateNewBookmarkPage() {
 
         const title = setData(currentContent?.title, content.title);
         const desc = setData(currentContent?.desc, content.desc);
-        bookmarkFormHook.setContent(title, desc, []);
+        const tags = bookmarkFormHook.getInputData().tags;
+
+        bookmarkFormHook.setContent(title, desc, tags);
     };
 
     const bookmarkFormHook = useBookmarkForm(
